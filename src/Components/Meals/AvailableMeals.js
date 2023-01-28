aa57d58d02191f5609bf0a9d6440bba96159077c
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-28 14:46:58 +0530

    mealItemComponentEfficientWay

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,55 +1,53 @@
-import classes from "./AvailableMeals.module.css"
-import MealItem from "./MealItem";
+import Card from "../UI/Card";
+import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
+const AvailableMeals = () => {
+  const MealsLists = DUMMY_MEALS.map((meal) => {
+    return (
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
+  });
 
-const AvailableMeals=()=>{
-
-    // const MealsLists=DUMMY_MEALS.map((meal)=>{
-    //    return(
-
-    //      <li className={cLASSES.meal}>
-    //       <h3 className={cLASSES.meal}>{meal.name}</h3>
-    //       <div className={cLASSES.description}>{meal.description}</div>
-    //       <div className={cLASSES.price}>{meal.price}</div>
-
-    //      </li>
-    //    )
-    // })
-    
-    
-    
-    return <section className={classes.meals}>
-        <ul>  
-            <MealItem items={DUMMY_MEALS}/>
-        </ul>
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{MealsLists}</ul>
+      </Card>
     </section>
-}
+  );
+};
 
 export default AvailableMeals;
