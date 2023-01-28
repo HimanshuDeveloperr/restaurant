2406e8146ccfbd3fd31d1b4e6c77003c8e4d082f
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-28 23:46:54 +0530

    efficientWayOfBuilsingMealInputForm

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,17 @@
 import React from "react";
-import Input from "./Input";
+import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css"
 const MealItemForm=()=>{
     return <form className={classes.form}>
-       <Input label="Amount" type="number"/>
+       <Input label="Amount" input={{
+        id:"amount",
+        type:"number",
+        min:"1",
+        max:"5",
+        step:"1",
+        defaultValue:"1"
+
+       }} />
         <button>+Add</button>
     </form>
 }
