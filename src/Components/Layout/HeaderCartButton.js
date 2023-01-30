f2908ec8c44e07604e636cdf63353cf6ba327e8e
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-31 02:37:48 +0530

    project finished

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,10 +1,11 @@
-import React ,{useContext}from "react";
+import React ,{useContext, useEffect, useState}from "react";
 import CartContext from "../../Store/CartContext";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css"
 
 const HeaderCartButton=(props)=>{
 
+    const[btnhigh,setbtnhigh]=useState(false)
     const context=useContext(CartContext);
 
     const numberOfItems=context.items.reduce((curNumber,item)=>{
@@ -12,7 +13,22 @@ const HeaderCartButton=(props)=>{
     },0)
     // reduce is a built in method that helps to convert array of datainto single value
 
-    return <button className={classes.button} onClick={props.onShow}>
+      const animation =`${classes.button} ${btnhigh ? classes.bump : ""}`
+const{items}=context;
+     useEffect(()=>{
+        if(items.length===0){
+            return
+        }
+        setbtnhigh(true)
+       const timer= setTimeout(() => {
+            setbtnhigh(false);
+          }, 300);
+          return ()=>{
+            clearTimeout(timer)
+          }
+     },[items])
+
+    return <button className={animation} onClick={props.onShow}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
