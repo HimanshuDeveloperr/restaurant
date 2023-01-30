42941c93d811494ad3ee96fb1f3c57040675ff45
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-30 13:19:43 +0530

    ProviderComponent

---
+++ src/Store/CartProvider.js
@@ -0,0 +1,10 @@
+import CartContext from "./CartContext";
+
+
+const CartProvider=(props)=>{
+  return  <CartContext.Provider>
+    {props.children}
+  </CartContext.Provider>
+}
+
+export default CartProvider;
